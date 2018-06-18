b31c581718dca4c508861eb3faa71c5882d29ed6
Trung Vo <vtt311096@gmail.com>
2018-06-18 10:16:36 -0400

    putting user name and profile image to navbar in MessageController

--- chat/Controller/MessagesController.swift
+++ chat/Controller/MessagesController.swift
@@ -40,9 +40,62 @@ class MessagesController: UITableViewController {
     func fetchUserAndSetupNavbarTitle() {
         Database.database().reference().child("users").child((Auth.auth().currentUser?.uid)!).observeSingleEvent(of: .value, with: { (snapshot) in
             if let dictionary = snapshot.value as? [String: AnyObject] {
-                self.navigationItem.title = dictionary["name"] as? String
+                let user = User()
+                user.name = dictionary["name"] as? String
+                user.email = dictionary["name"] as? String
+                user.profileImageUrl = dictionary["profileImageUrl"] as? String
+                self.setupNavBarWithUser(user: user)
             }
         }, withCancel: nil)
+    }
+    
+    func setupNavBarWithUser(user: User) {
+        // wrapper
+        let titleView = UIView()
+        titleView.backgroundColor = UIColor.yellow
+        titleView.frame = CGRect(x: 0, y: 0, width: 200, height: 80)
+        
+        // profile image view
+        let profileImageView = UIImageView()
+        profileImageView.contentMode = .scaleAspectFill
+        profileImageView.layer.cornerRadius = 16
+        profileImageView.clipsToBounds = true
+        profileImageView.translatesAutoresizingMaskIntoConstraints = false
+        if user.profileImageUrl != nil {
+            profileImageView.loadImageUsingCacheWithURL(urlString: user.profileImageUrl!)
+        }
+        
+        // user name label view
+        let nameLabel = UILabel()
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.text = user.name
+        nameLabel.font = UIFont.boldSystemFont(ofSize: 13)
+        
+        // container
+        let container = UIView()
+        container.translatesAutoresizingMaskIntoConstraints = false
+        container.addSubview(profileImageView)
+        container.addSubview(nameLabel)
+        titleView.addSubview(container)
+        
+        container.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        container.centerXAnchor.constraint(equalTo: titleView.centerXAnchor).isActive = true
+        container.centerYAnchor.constraint(equalTo: titleView.centerYAnchor).isActive = true
+        
+        profileImageView.centerXAnchor.constraint(equalTo: container.centerXAnchor).isActive = true
+        profileImageView.centerYAnchor.constraint(equalTo: container.centerYAnchor).isActive = true
+        profileImageView.topAnchor.constraint(equalTo: container.topAnchor, constant: 10).isActive = true
+        profileImageView.heightAnchor.constraint(equalTo: container.heightAnchor, multiplier: 0.4).isActive = true
+        profileImageView.widthAnchor.constraint(equalToConstant: 32).isActive = true
+        
+        nameLabel.centerXAnchor.constraint(equalTo: container.centerXAnchor).isActive = true
+        nameLabel.centerYAnchor.constraint(equalTo: container.centerYAnchor).isActive = true
+        nameLabel.bottomAnchor.constraint(equalTo: container.bottomAnchor, constant: -12).isActive = true
+        nameLabel.heightAnchor.constraint(equalTo: container.heightAnchor, multiplier: 0.4).isActive = true
+        nameLabel.widthAnchor.constraint(equalTo: container.widthAnchor).isActive = true
+        
+        self.navigationItem.titleView = titleView
+        
     }
     
     @objc func handleLogout() {
